f1bf3afdcf92e62dc3eb032f310761bc1220280f
Etuitive Apps <namoricoe@gmail.com>
2015-04-06 19:08:55 -0500

    Refactor constructor

--- src/com/etuitiveapps/javacoderbytetests/FirstReverseClassTests.java
+++ src/com/etuitiveapps/javacoderbytetests/FirstReverseClassTests.java
@@ -2,15 +2,21 @@ package com.etuitiveapps.javacoderbytetests;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.etuitiveapps.javacoderbyte.FirstReverseClass;
 
 public class FirstReverseClassTests {
+  private FirstReverseClass reverse;
+
+  @Before
+  public void setup() {
+    reverse = new FirstReverseClass();
+  }
 
   @Test
   public void reverseWordLong() {
-    FirstReverseClass reverse = new FirstReverseClass();
     String input = "long";
     String expected = "gnol";
     String actual = reverse.FirstReverse(input);
@@ -19,7 +25,6 @@ public class FirstReverseClassTests {
 
   @Test
   public void reversecoderbyte() {
-    FirstReverseClass reverse = new FirstReverseClass();
     String input = "coderbyte";
     String expected = "etybredoc";
     String actual = reverse.FirstReverse(input);
@@ -28,7 +33,6 @@ public class FirstReverseClassTests {
 
   @Test
   public void reverseI_Love_Code() {
-    FirstReverseClass reverse = new FirstReverseClass();
     String input = "I Love Code";
     String expected = "edoC evoL I";
     String actual = reverse.FirstReverse(input);
